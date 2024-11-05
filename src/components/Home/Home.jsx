e33c7a5958a11ed6d8628ff16fe244efc3a55f20
Aashik1701 <md1704ashik@gmail.com>
2024-11-06 01:20:20 +0530

    First commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,9 @@ const Home = () => {
     <div className="h-screen flex items-center justify-center bg-black text-white font-bold">
       <div className="relative">
         <div className="absolute inset-0 bg-green-500 opacity-20 blur-lg"></div>
-        <div className="relative">
-          <h1 className="text-8xl font-extrabold tracking-wide">CODE</h1>
-          <h2 className="text-7xl font-extrabold tracking-wide text-green-500">GREEN</h2>
+        <div className="relative items-center">
+          <h1 className="text-8xl font-extrabold font-sans tracking-wide">CODE</h1>
+          <h2 className="text-8xl font-light tracking-wide text-green-500">GREEN</h2>
         </div>
       </div>
     </div>
