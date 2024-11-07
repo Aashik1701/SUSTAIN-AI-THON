92ee9efac689a5bd9e8e8e721b1b1714d2194407
notnaveen222 <notnaveen2@gmail.com>
2024-11-07 20:23:52 +0530

    About Section added, Naveen

---
+++ src/components/About/AboutBG.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutBG = () => {
+  return (
+    <div className="flex flex-row justify-between w-screen h-screen overflow-x-hidden -z-10 dark-bg">
+      <div className="h-[550px] w-[700px] rounded-full yellow-ellipse-bg blur-[300px] opacity-80  mt-[100px] relative right-[200px]"></div>
+      <div className="h-[550px] w-[700px] rounded-full green-ellipse-bg blur-[300px] opacity-80  relative bottom-[200px] left-[100px]"></div>
+    </div>
+  );
+};
+
+export default AboutBG;
