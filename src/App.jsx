a478946f8cdba311884ce3de314ed99d1c038ae3
Aashik1701 <md1704ashik@gmail.com>
2024-11-07 22:30:21 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 //import RegisterButton from './components/Register/RegisterButton';
 import About from "./components/About/About";
-import Themes from "./components/Themes/Themes";
+//import Themes from "./components/Themes/Themes";
 import Sponsors from "./components/Sponsors/Sponsors";
 import Rounds from "./components/Rounds/Rounds";
 import CoreTeam from "./components/CoreTeam/CoreTeam";
@@ -15,7 +15,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
-      <Themes />
+ 
       <Sponsors />
       <Rounds />
       <CoreTeam />
