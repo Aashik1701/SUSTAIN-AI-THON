0b3bdc9ab7a6d43dc4ad07e0e3b2b2cb5aab7800
legendtharun <manikandan211205@gmail.com>
2024-11-08 20:22:14 +0530

    added themes and sponsors section enhanced

---
+++ src/components/Themes/Card.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Card = ({img,theme}) => {
+  return (
+    <div className='flex flex-col align-middle justify-center max-w-[446px] border-2 border-white rounded-lg'>
+        <div className='w-full h-[492px] rounded-[32px]'>
+            <img src={img} className='w-full h-full'/>
+        </div>
+        <div className='flex align-middle justify-center'>
+            <h3 className='inter-semibold text-xl uppercase'>{theme}</h3>
+        </div>
+    </div>
+  )
+}
+
+export default Card
--- src/components/Themes/Themes.jsx
+++ src/components/Themes/Themes.jsx
@@ -1,9 +1,14 @@
-
+import Card from "./Card";
+import Checker from "../../assets/checker.png"
 const Themes = () => {
   return (
-    <section id="themes" className="p-8 text-white">
-      <h2 className="text-3xl-white font-semibold">Themes</h2>
-      <p className="mt-4 text-white">Explore various themes such as AI, sustainability, and more...</p>
+    <section id="themes" className="p-3 md:p-7 lg:p-10 text-white flex flex-col align-middle justify-center">
+      <h1 className="julius-sans-one-regular text-6xl md:text-5xl lg:text-6xl text-[#00AEAD] tracking-wider">THEMES</h1>
+     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 p-10">
+      <Card img={Checker} theme={"theme 1"}/>
+      <Card img={Checker} theme={"theme 2"}/>
+      <Card img={Checker} theme={"theme 3"}/>
+     </div>
     </section>
   );
 };
