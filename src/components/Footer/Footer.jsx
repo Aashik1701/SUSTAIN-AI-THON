ba21c492a6d125575f8ecd6ee0d952df79618d19
Aashik1701 <md1704ashik@gmail.com>
2024-11-15 00:50:46 +0530

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
         <div className="text-center sm:text-left mb-4 sm:mb-0">
           <h3 className="text-2xl md:text-3xl lg:text-4xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-green-400 to-green-700">Sustain-Ai-Thon 2024</h3>
           <p className="text-sm sm:text-base">
-            © 2023 Sustain-Ai-Thon. All rights reserved.
+            © 2024 Sustain-Ai-Thon. All rights reserved.
           </p>
         </div>
         <div className="text-center sm:text-right">
