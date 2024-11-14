95d922edb1c68bd6f874b65166b4500664df01d2
Aashik1701 <md1704ashik@gmail.com>
2024-11-14 22:38:13 +0530

    update

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,5 +1,8 @@
 import { useState, useEffect } from 'react';
 import gone from '../../assets/Core/gallery/g1.jpeg';
+import gtwo from '../../assets/Core/gallery/gtwo.jpg';
+import gthree from '../../assets/Core/gallery/gthree.jpg';
+import aicgroup from '../../assets/Core/gallery/aicgroup.jpeg';
 const Gallery = () => {
   const images = [
     {
@@ -8,19 +11,19 @@ const Gallery = () => {
       thumbnail: gone
     },
     {
-      url: "/src/assets/Core/gallery/gtwo.jpg",
+      url: gtwo,
       alt: "Presentation Session",
-      thumbnail: "src/assets/Core/gallery/gtwo.jpg"
+      thumbnail: gtwo
     },
     {
-      url: "src/assets/Core/gallery/gthree.png",
+      url: gthree,
       alt: "Workshop Session",
-      thumbnail: "src/assets/Core/gallery/gthree.png"
+      thumbnail: gthree
     },
     {
-      url: "src/assets/Core/gallery/aicgroup.jpeg",
+      url: aicgroup,
       alt: "Team Discussion",
-      thumbnail: "/src/assets/Core/gallery/aicgroup.jpeg"
+      thumbnail: aicgroup
     },
     {
       url: "/api/placeholder/1200/600",
