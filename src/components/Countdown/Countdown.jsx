8a222bcaef1040fdf641ace7adb3982c457ccc83
Aashik1701 <md1704ashik@gmail.com>
2024-12-29 19:36:32 +0530

    CountDown Updated

--- src/components/Countdown/Countdown.jsx
+++ src/components/Countdown/Countdown.jsx
@@ -3,6 +3,7 @@ import { motion, useInView } from "framer-motion";
 
 const Countdown = ({ targetDate }) => {
   const calculateTimeLeft = () => {
+    const targetDate = new Date('2025-01-17T00:00:00');
     const difference = new Date(targetDate) - new Date();
     let timeLeft = {};
 
